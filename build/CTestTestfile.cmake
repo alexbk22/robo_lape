94ab21a6e1ec109656ae7bebb27eb12b65c6fa4f
alexbk22 <alexbkraemer@gmail.com>
2018-07-10 15:54:44 -0300

    Com catkin_make

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/git/robo_lape/src
+# Build directory: /home/pi/git/robo_lape/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
