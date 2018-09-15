076f540aac3d7d544b2769ab3eeb459bdff851f8
Kauê de Moraes Vestena <kauemv2@gmail.com>
2018-09-14 21:58:22 -0300

    Update tf_robo_laser_tf2.py

--- scripts/tf_robo_laser_tf2.py
+++ scripts/tf_robo_laser_tf2.py
@@ -13,4 +13,12 @@ import geometry_msgs.msg
 
 def laser_relative_pose():
     rospy.init_node('laser_relative_pose', anonymous=False)
+    br = tf2_ros.TransformBroadcaster()
     
+    t.transform.translation = (-0.07,0.0,0.545)
+    
+    q = tf_conversions.transformations.quaternion_about_axis(-math.pi * 0.5, (0, 1, 0))
+    t.transform.rotation.x = q[0]
+    t.transform.rotation.y = q[1]
+    t.transform.rotation.z = q[2]
+    t.transform.rotation.w = q[3]
