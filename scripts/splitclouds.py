f5af427504d6e9bf7ed9da6702adfb6b5d911b89
kauemv2@gmail.com <kauemv2@gmail.com>
2018-10-04 10:31:51 -0300

    POINT CLOUDS, FINALLY

--- scripts/splitclouds.py
+++ scripts/splitclouds.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 
-file = open("/home/kaue2/git/robo_lape/scripts/cloud.txt","r")
+import os
+
+file = open(str(os.environ['HOME'])+"/git/robo_lape/clouds/cloud.txt","r")
+# /home/kaue/git/robo_lape/clouds/cloud.txt
+
 
 count = 5000
 
+n = 0
+i = 0
+
+# temp_out = open(str(os.environ['HOME'])+"/git/robo_lape/clouds/cloud1/cloud1_"+str(n)+".txt","w")
+
+
 for line in file:
-    out
+    if (i%count == 0):
+        n = n + 1
+        temp_out = open(str(os.environ['HOME'])+"/git/robo_lape/clouds/cloud1/cloud1_"+str(n)+".txt","w")
+        print n
+
+    temp_out.write(line)
+    i += 1
